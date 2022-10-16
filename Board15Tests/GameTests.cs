311c5cf019c2d9e5fcb8621591327f3fe850d4b4
WIN-P2T2Q32BFT7\User <dmitryprogrammer90@gmail.com>
2022-10-16 03:11:09 +0300

    Добавьте файлы проекта.

---
+++ Board15Tests/GameTests.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Board15;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Board15.Tests
+{
+    [TestClass()]
+    public class GameTests
+    {
+
+        [TestMethod()]
+        public void StartTest()
+        {
+            Game game = new Game(4);
+            game.Start();
+            Assert.AreEqual(1, game.GetDigitAt(0, 0));
+            Assert.AreEqual(2, game.GetDigitAt(1, 0));
+            Assert.AreEqual(3, game.GetDigitAt(2, 0));
+            Assert.AreEqual(4, game.GetDigitAt(3, 0));
+            Assert.AreEqual(5, game.GetDigitAt(0, 1));
+            Assert.AreEqual(6, game.GetDigitAt(1, 1));
+            Assert.AreEqual(7, game.GetDigitAt(2, 1));
+            Assert.AreEqual(8, game.GetDigitAt(3, 1));
+            Assert.AreEqual(9, game.GetDigitAt(0, 2));
+            Assert.AreEqual(10, game.GetDigitAt(1, 2));
+            Assert.AreEqual(11, game.GetDigitAt(2, 2));
+            Assert.AreEqual(12, game.GetDigitAt(3, 2));
+            Assert.AreEqual(13, game.GetDigitAt(0, 3));
+            Assert.AreEqual(14, game.GetDigitAt(1, 3));
+            Assert.AreEqual(15, game.GetDigitAt(2, 3));
+            Assert.AreEqual(0, game.GetDigitAt(3, 3));
+        }
+
+        [TestMethod()]
+        public void PressAtTest()
+        {
+            Game game = new Game(4);
+            game.Start();
+            game.PressAt(2,3);
+            Assert.AreEqual(0, game.GetDigitAt(2, 3));
+            Assert.AreEqual(15, game.GetDigitAt(3, 3));
+            game.PressAt(2, 2);
+            Assert.AreEqual(0, game.GetDigitAt(2, 2));
+            Assert.AreEqual(11, game.GetDigitAt(2, 3));
+        }
+
+        [TestMethod()]
+        public void GetDigitAtTest()
+        {
+            Game game = new Game(4);
+            game.Start();
+            game.PressAt(2, 3);
+            Assert.AreEqual(0, game.GetDigitAt(-5, -34));
+            Assert.AreEqual(0, game.GetDigitAt(15, 6));
+        }
+
+        [TestMethod()]
+        public void SolvedTest()
+        {
+            Game game = new Game(4);
+            game.Start();
+            Assert.IsTrue(game.Solved());
+            game.PressAt(2, 3);
+            Assert.IsFalse(game.Solved());
+            game.PressAt(3, 3);
+            Assert.IsTrue(game.Solved());
+        }
+    }
+}
---
+++ Board15Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью указанного ниже 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// общие сведения об этой сборке.
+[assembly: AssemblyTitle("Board15Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Board15Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка параметра ComVisible в значение false делает типы в этой сборке невидимыми 
+// для компонентов модели COM.  Если тип в этой сборке необходимо сделать доступным 
+// модели COM, задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("19bc7245-10cb-40d9-9958-910885721185")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [сборка: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
